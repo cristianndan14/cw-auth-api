4113055d2eb578b1f3b96f63b5538a5964948a7e
Cristian Ance <105828903+cristianndan14@users.noreply.github.com>
2023-08-03 14:17:12 -0300

    add global CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -5,6 +5,7 @@ from connexion.resolver import MethodViewResolver
 from swagger_server import encoder
 from swagger_server.resources.db import db
 from swagger_server.config.access import access
+from flask_cors import CORS
 
 config = access()
 
@@ -16,6 +17,7 @@ def main():
                 resolver=MethodViewResolver("swagger_server.controllers"))
     app.app.config["SQLALCHEMY_DATABASE_URI"] = config.get("SQLALCHEMY_DATABASE_URI")
     db.init_app(app.app)
+    CORS(app.app, resources={r"/*": {"origins": "*"}})
     app.run(host="0.0.0.0", port=5001, debug=True)
 
 
