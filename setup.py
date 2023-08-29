f1da836f6db0ba1b3f10db415c31f4e5098f65b0
Cristian Ance <105828903+cristianndan14@users.noreply.github.com>
2023-08-29 18:08:15 -0300

    url updated

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ REQUIRES = [
 setup(
     name=NAME,
     version=VERSION,
-    description="auth-api",
+    description="cw-auth-api",
     author_email="",
     url="",
-    keywords=["Swagger", "auth-api"],
+    keywords=["Swagger", "cw-auth-api"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={'': ['swagger/swagger.yaml']},
